1f2bdc6817d1bc918c0a1c1911ae2a1fb577419c
Romain L <romain.leger2@gmail.com>
2014-12-09 18:02:18 +0100

    update 12/09

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // JavaScript Document
+
+$(document).ready(function(){
+	
+	//  Homepage size
+    $('#home-top-image').height($(window).height());
+	
+$('.domestic-travel a, .investment a').on('click', function(){
+   var target = $(this).attr('rel');
+   $("#"+target).show().siblings("div").hide();
+   $(this).addClass("active-tab").removeClass("tab").siblings(this).addClass("tab").removeClass("active-tab");
+   return false;
+});
+	
+
+	
+});
