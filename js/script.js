7148d3f53b45b1d0cd4b7a175b54e131d32a7907
Romain L <romain.leger2@gmail.com>
2014-12-10 22:58:12 +0100

    update 12/10

--- js/script.js
+++ js/script.js
@@ -11,7 +11,13 @@ $('.domestic-travel a, .investment a').on('click', function(){
    $(this).addClass("active-tab").removeClass("tab").siblings(this).addClass("tab").removeClass("active-tab");
    return false;
 });
-	
-
-	
+	$('.popout-menu-wrapper').hover(function(){
+		$(this).children('.sub-menu').toggle();
+		});
+		
+		$('.menu-hover, .popout-menu').hover(function () {
+		$('.popout-menu').toggle();
+		$('.navbar-header').toggleClass('grey-navbar-header');
+		$('.navbar-brand').toggleClass('navbar-brand-hover');
+		});
 });
